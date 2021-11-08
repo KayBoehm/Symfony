716d1d0d27dab3d61fb54d44c1815c3098668ba3
Kay <kayboehm@gmx.de>
2021-11-08 14:27:56 +0100

    Jetzt auch noch Twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController{
+class QuestionController extends AbstractController
+{
 
     /**
      * @Route("/")
      */
-    public function homepage(){
+    public function homepage()
+    {
         return new Response('What a bewitching controller we have conjured!');
     }
 
@@ -17,12 +21,20 @@ class QuestionController{
     /**
      * @Route("/questions/{slug}")
      */
-    public function show($slug){
-        return new Response(sprintf(
-            'Future page to show a question "%s" .',
-       ucwords(str_replace('-',' ', $slug))
+    public function show($slug)
+    {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still XD',
+            'Hoestly, I like furry shoes better than my cat',
+            'Maybe... try saying the spell backwards?',
+        ];
 
-    ));
-        
+        return $this->render('question/show.html.twig', [
+            //ucwords versieht alle Wörter mit Uppper Case
+            //str_replace(search, replace, subject)
+            'question'=> ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+
+        ]);
     }
 }
